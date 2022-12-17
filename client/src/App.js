b03a018b4636efd04e932d9a61b015a6617b28f8
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-12-17 11:31:44 +0530

    Tailwind Initialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
     </div>
-  );
+  )
 }
-
-export default App;
