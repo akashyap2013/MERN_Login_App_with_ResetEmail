6c8fcbc66c3b8a1dcaa8424ce9154a5d346fc4a2
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-12-17 12:34:29 +0530

    Username Component Added

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import avatar from '../assets/profile.png';
+
+import styles from '../styles/Username.module.css';
 
 export default function Username() {
   return (
-    <div>Username</div>
+    <div className="container mx-auto">
+      <div className='flex justify-center items-center h-screen'>
+        <div className={styles.glass}>
+
+          <div className="title flex flex-col items-center">
+            <h4 className='text-5xl font-bold'>Hello Again!</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+              Explore More by connecting with us.
+            </span>
+          </div>
+
+          <form className='py-1'>
+              <div className='profile flex justify-center py-4'>
+                  <img src={avatar} className={styles.profile_img} alt="avatar" />
+              </div>
+
+              <div className="textbox flex flex-col items-center gap-6">
+                  <input className={styles.textbox} type="text" placeholder='Username' />
+                  <button className={styles.btn} type='submit'>Let's Go</button>
+              </div>
+
+              <div className="text-center py-4">
+                <span className='text-gray-500'>Not a Member <Link className='text-red-500' to="/register">Register Now</Link></span>
+              </div>
+
+          </form>
+
+        </div>
+      </div>
+    </div>
   )
 }
