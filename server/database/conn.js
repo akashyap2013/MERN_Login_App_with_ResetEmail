43fd14f49e6229945c27ad4b32a30782ebb6ff92
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-12-18 17:39:56 +0530

    Mongodb ATLAS Added

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 import { MongoMemoryServer } from "mongodb-memory-server";
+import ENV from '../config.js'
 
 async function connect(){
+
     const mongod = await MongoMemoryServer.create();
     const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true)
-    const db = await mongoose.connect(getUri);
+    // const db = await mongoose.connect(getUri);
+    const db = await mongoose.connect(ENV.ATLAS_URI);
     console.log("Database Connected")
     return db;
 }
